4db81c38e9d9ad9857be1868b520104c99b1f43a
XinweiYuan <72956686+XinweiYuan@users.noreply.github.com>
2020-11-13 11:13:41 +0800

    Create Code.cpp

---
+++ 226. 翻转二叉树/Code.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root)
+            return nullptr;
+        TreeNode* newRoot = new TreeNode(root->val);
+        newRoot->left = invertTree(root->right);
+        newRoot->right = invertTree(root->left);
+        return newRoot;
+    }
+};
