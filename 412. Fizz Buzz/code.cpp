df856d38fbaf8e8d1a5d21fcdec805ea4d0cecac
XinweiYuan <72956686+XinweiYuan@users.noreply.github.com>
2021-01-03 12:17:22 +0800

    Create code.cpp

---
+++ 412. Fizz Buzz/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> rtn;
+        for (int i = 1; i <= n; ++i) {
+            if (i%3==0 && i%5==0) 
+                rtn.push_back("FizzBuzz");
+            else if (i%3==0)
+                rtn.push_back("Fizz");
+            else if (i%5==0)
+                rtn.push_back("Buzz");
+            else
+                rtn.push_back(to_string(i));
+        }
+        return rtn;
+    }
+};
